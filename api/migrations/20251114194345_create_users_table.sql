a24002233ae7167ba3e030d7c2c8d3c21a11e40c
alzakharov <git@alzakharov.com>
2025-11-15 12:31:17 +0200

    feat: roles system WIP

--- api/migrations/20251114194345_create_users_table.sql
+++ api/migrations/20251114194345_create_users_table.sql
@@ -1,15 +1,17 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE TABLE messages
+CREATE TABLE users
 (
     id         INT AUTO_INCREMENT PRIMARY KEY COMMENT 'identifier',
-    chat_id    INT  NOT NULL,
-    user_id    INT  NOT NULL,
-    text       TEXT NOT NULL,
+    email      VARCHAR(1000) NOT NULL COMMENT 'email',
+    password   VARCHAR(1000) NOT NULL COMMENT 'password',
+    nickname   VARCHAR(1000) NOT NULL COMMENT 'nickname',
+    role       VARCHAR(100)  NOT NULL COMMENT 'user role',
+    token      VARCHAR(1000) COMMENT 'token',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT 'creation date',
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'update date',
-    FOREIGN KEY (chat_id) REFERENCES chats (id),
-    FOREIGN KEY (user_id) REFERENCES users (id)
+    UNIQUE KEY uq_users_email (email),
+    UNIQUE KEY uq_users_nickname (nickname)
 );
 -- +goose StatementEnd
 
